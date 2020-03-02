6019d767b1e675b295e69c37e02cb8221ade8dbb
EvgeniyPC <logvinov.evgeniy@yandex.ru>
2020-03-03 01:13:31 +0300

    1st Commit

--- src/task803.java
+++ src/task803.java
@@ -9,10 +9,27 @@ public class task803 {
     Scanner sc;
     public void run() throws IOException{
         sc = new Scanner(new File("input.txt"));
-        int a=sc.nextInt();
         pw = new PrintWriter(new File("output.txt"));
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int k = sc.nextInt();
+
+        int i = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        while (i < k) {
+            list.add(digit(a, b));
+            a = a * 10 - digit(a, b) * b;
+            i++;
+        }
+        if (a > b )
+            pw.print(list.get(k));
+        else pw.print(list.get(k - 1));
         pw.close();
     }
 
+    public int digit(int a, int b)
+    {
+        return (a * 10 / b);
+    }
 
 }
